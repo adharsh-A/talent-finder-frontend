73d51a5820d479e1af002b197affd745cf14ea12
adharsh-A <103622525+adharsh-A@users.noreply.github.com>
2024-10-27 18:36:57 +0530

    after vercel

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc"; // Using SWC for faster builds
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   css: {
-    postcss: "./postcss.config.js", // Specify PostCSS configuration
+    postcss: "./postcss.config.js",
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // Set up alias for src directory
-      "@components": path.resolve(__dirname, "./src/components"), // Alias for components
-      "@assets": path.resolve(__dirname, "./src/assets"), // Alias for assets
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
-  plugins: [react()], // Include React plugin
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          three: ['three', '@react-three/fiber', '@react-three/drei'],
+        }
+      }
+    }
+  },
+  plugins: [react()]
 });
