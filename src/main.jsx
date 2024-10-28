2c36df2063db988f6776c69df7590cd3705ec673
adharsh-A <103622525+adharsh-A@users.noreply.github.com>
2024-10-28 08:59:54 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,38 @@ import { createRoot } from "react-dom/client";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import "./global.css";
+import { ThemeProvider, createTheme } from "@mui/material";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import { StyledEngineProvider } from '@mui/material/styles';
 
+
+import "./global.css";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff0000",
+    },
+    secondary: {
+      main: "#00ff00",
+    },
+  },
+});
+const cache = createCache({
+  key: "css",
+  prepend: true,
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <StyledEngineProvider injectFirst>
+    <CacheProvider value={cache}>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      </ThemeProvider>
+      </CacheProvider>
+      </StyledEngineProvider>
   </StrictMode>
 );
